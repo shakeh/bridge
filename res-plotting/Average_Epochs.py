8a126c56acf829a558dbdb15877dbe9e0e3898eb
fkeri <fkeri@fandm.edu>
2015-06-19 14:32:49 -0400

    Update Average_Epochs.py

--- res-plotting/Average_Epochs.py
+++ res-plotting/Average_Epochs.py
@@ -1,5 +1,12 @@
 # Filip Keri
-# separate all the TOAs into epochs (bins), compress all the data in an epoch into one point
+# Average_Epochs.py
+# A script that takes in a .par file, a .tim file, and an output directory
+# as a result, it creates a new file with TOAs compressed in epochs, stored in output directory
+# sample input:
+# python Average_Epochs.py /Users/fkeri/Desktop/B1855+09_NANOGrav_9yv0.par /Users/fkeri/Desktop/B1855+09_NANOGrav_9yv0.tim /Users/fkeri/Desktop/
+# we can see that it takes in 3 line arguments: [INPUT .par FILE], [INPUT .tim FILE], [OUTPUT DIRECTORY]
+# the output file will have the same name as the input file, with "Output_for_plotting_" as a prefix: "Output_for_plotting_B1855+09_NANOGrav_9yv0.tim"
+# it is possible to name the output file differently by putting the file name in [OUTPUT DIRECTORY]: /Users/fkeri/Desktop/filename.tim
 
 import sys
 import math
@@ -13,12 +20,13 @@ def freq_idx( F ):
 		if ( F >= freq[ i ][ 0 ] ) and ( F <= freq[ i ][ 1 ] ):
 			return i
 	return 0
+	
 F = [ 350, 450, 800, 1400, 2300 ]
 
 output_dir = str(sys.argv[ 3 ])
 psr = T.tempopulsar( parfile = str( sys.argv[ 1 ] ), timfile = str( sys.argv[ 2 ] ) )
 sort_cmp = N.argsort( psr.stoas )
-bin = [] #format: residuals, error, TOA, frequency
+bin = [] # format: residuals, error, TOA, frequency
 
 # sorting residuals, errors, frequencies, and TOAs by TOAs
 Residuals = psr.residuals()[ sort_cmp ]
@@ -67,9 +75,11 @@ for i in range( len( bin ) ):
 pulsar_path = str( sys.argv[ 1 ] )
 pulsar_file = pulsar_path.split('/')
 pulsar_name = pulsar_file[-1].split('.')
-print "*****" + pulsar_name[0]
-print sys.argv
-outFile = open( str(sys.argv[ 3 ]) + pulsar_name[0] + "_Output_for_plotting.tim", "w" )
+
+if sys.argv[3][-4] != '.':
+    outFile = open( os.path.join( str(sys.argv[ 3 ]), "Output_for_plotting_"+pulsar_name[0]+".tim", "w" )
+else:
+    outFile = open( sys.argv[3], "w" )
 
 outFile.write( psr.name+"\n" )
 outFile.write( "This program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it\n" )
