c5d008c4918f26b83195a2f636458ad843e750b1
fkeri <fkeri@fandm.edu>
2015-06-19 13:44:22 -0400

    Update TOA_Filtering.py

--- filtering/TOA_Filtering.py
+++ filtering/TOA_Filtering.py
@@ -40,42 +40,40 @@ def transform( X ):
 	return float( X )
 
 inFile = open( sys.argv[1], "r" )
-outFile = open( os.path.join( sys.argv[2], "TOAfiltered_"+sys.argv[1].split("/")[-1], "w" )
+
+save_path = sys.argv[4]
+nameFile = os.path.join( save_path, "TOAfiltered"+sys.argv[1].split("/")[-1] )
+outFile = open( nameFile, "w" )
 
 inFile.readline() #omit first line
 ALLlines = inFile.readlines()
 
-ok = False
-print( "Enter starting and ending date (MJD or YYYY/MM/DD format):" )
-while not ok:
-	start = str( raw_input( "Start: " ) )
-	end = str( raw_input( "End: " ) )
-	start = transform( start )
-	end = transform( end )
+start = str( sys.argv[2] )
+end = str( sys.argv[3] )
+start = transform( start )
+end = transform( end )
 
-	if ( start == "not ok" ) or ( end == "not ok" ):
-		print( "Wrong format! Please enter the data again!" )
+if ( start == "not ok" ) or ( end == "not ok" ):
+	outFile.write( "Wrong format! Please enter the data again! (either MJD or YYYY/MM/DD format)" )
+else:
+	if ( start < 0 ) or ( end < 0 ):
+		outFile.write( "Your starting and ending points cannot be less than zero! Please enter the data again! (either MJD or YYYY/MM/DD format)" )
 	else:
-		if ( start < 0 ) or ( end < 0 ):
-			print( "Your starting and ending points cannot be less than zero! Please enter the data again!" )
+		if ( start > end ):
+			outFile.write( "Your starting point cannot be greater than your ending point! Please enter the data again! (either MJD or YYYY/MM/DD format)" )
 		else:
-			if ( start > end ):
-				print( "Your starting point cannot be greater than your ending point! Please enter the data again!" )
-			else:
-				ok = True
+			L = []
+			for i in range( 0, len( ALLlines ) ):
+			    L.append( ALLlines[i].split(' ') )
+			#L.sort(key = lamda row: row[2] )
 
-L = []
-for i in range( 0, len( ALLlines ) ):
-    L.append( ALLlines[i].split(' ') )
-#L.sort(key = lamda row: row[2] )
-
-for i in range( 0, len( ALLlines ) ):
-	if ( len( L[i] ) > 2 ):
-		if isFloat( L[i][2] ):
-			TOA = float( L[i][2] )
-			if(TOA >= start and TOA <= end ):
-				X = ' '.join( L[i] )
-				outFile.write( X )
+			for i in range( 0, len( ALLlines ) ):
+				if ( len( L[i] ) > 2 ):
+					if isFloat( L[i][2] ):
+						TOA = float( L[i][2] )
+						if(TOA >= start and TOA <= end ):
+							X = ' '.join( L[i] )
+							outFile.write( X )
 
 inFile.close()
 outFile.close()
