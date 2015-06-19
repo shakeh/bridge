224a25d4ddf4b613a0236f52c7c5695149d5be2a
fkeri <fkeri@fandm.edu>
2015-06-19 14:18:31 -0400

    Update Turn_Off_Fitting.py

--- filtering/Turn_Off_Fitting.py
+++ filtering/Turn_Off_Fitting.py
@@ -1,11 +1,21 @@
+# Turn_Off_Fitting.py
+# A script that takes in a .par file, and an output directory
+# as a result, it creates a new .par file with fit option turned OFF
+# sample input:
+# python Turn_Off_Fitting.py /Users/fkeri/Desktop/B1855+09_NANOGrav_9yv0.par /Users/fkeri/Desktop/
+# we can see that it takes in 2 line arguments: [INPUT FILE], [OUTPUT DIRECTORY]
+# the output file will have the same name as the input file, with "NoFit_" as a prefix: "NoFit_B1855+09_NANOGrav_9yv0.par"
+# it is possible to name the output file differently by putting the file name in [OUTPUT DIRECTORY]: /Users/fkeri/Desktop/filename.par
+
 import sys
 import os.path
 
 inputFileName = sys.argv[1]
 save_path = sys.argv[2]
-outputFileName = os.path.join( save_path, "NoFit_"+sys.argv[1].split("/")[-1] )
-
-
+if save_path[-4] != '.':
+    outputFileName = os.path.join( save_path, "NoFit_"+sys.argv[1].split("/")[-1] )
+else:
+    outputFileName = save_path
 
 with open(inputFileName,'r') as readFile:
     with open(outputFileName,'w') as writeFile:
