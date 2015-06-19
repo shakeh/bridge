2bccc377191cf2fd12c9829178c00b8d15f9a04b
fkeri <fkeri@fandm.edu>
2015-06-19 13:31:33 -0400

    Update and rename frequency.py to Frequency_Filtering.py

--- filtering/Frequency_Filtering.py
+++ filtering/Frequency_Filtering.py
@@ -10,7 +10,7 @@ start = float( sys.argv[2] )
 end = float( sys.argv[3] ) 
 
 save_path = sys.argv[4]
-nameFile = os.path.join( save_path, "Range.tim")
+nameFile = os.path.join( save_path, "FreqRange"+sys.argv[1].split("/")[-1] )
 outFile = open( nameFile, "w" )
 
 L = []
